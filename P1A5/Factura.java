50b6cebda8c5010af7c0ef051d3331151d871d81
aleparrra24-ui <aleparrra24@gmail.com>
2026-08-29 17:51:01 -0600

    Add files via upload

---
+++ P1A5/Factura.java
@@ -0,0 +1,21 @@
+
+public class Factura
+{
+    //Atributo (Valores instacias sinónimo de atributos)
+	String numPieza;
+	String descripcion;
+	int cantidad;
+	double precio;
+	
+	Factura() { //Cosntructor que inicializa las 4 variables instancias --- un constructor no lleva void
+	    numPieza = "";
+	    descripcion = "";
+	    cantidad = 0;
+	    precio = 0.0;
+	}
+	
+	//Cuando un método pide mostrar un dato --- se usa double y no void-- con void no lleva return
+	double obtenerMontoFactura() {
+	    return cantidad*precio;
+	}
+}
